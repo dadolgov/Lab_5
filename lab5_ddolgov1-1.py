5cb647da0c2792d0345ec627e63b0bbc7c601b1a
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-13 20:27:02 -0500

    Improved the output, renamed the file

---
+++ lab5_ddolgov1-1.py
@@ -0,0 +1,42 @@
+"""
+Dice game
+Author: Dmitrii Dolgov
+Roll 2 dice, count the scores and print the funny names of combinations
+"""
+
+import random
+
+die_1 : int = random.randint(1,6)
+die_2 : int = random.randint(1,6)
+total : int = die_1 + die_2
+
+#print(die_1)
+#print(die_2)
+
+print(f"you rolled {total}!")
+print(f"your dice are:")
+if total==12: 
+    print(f"{die_1} & {die_2}: Boxcars!")
+elif total==11:
+    print(f"{die_1} & {die_2}: Six Five no Jive!")
+elif total==10 and die_1==5:
+    print(f"{die_1} & {die_2}: Puppy Paws!")
+elif total==9:
+    print(f"{die_1} & {die_2}: Nina from Pasadena!")
+elif total==8 and die_1==4:
+    print(f"{die_1} & {die_2}: Eighter from Decatur!")
+elif total==7 and (die_1==1 or die_2==1):
+    print(f"{die_1} & {die_2}: Six Ace")
+elif total==6 and die_1==3:
+    print(f"{die_1} & {die_2}: Jimmy Hicks from the Sticks!")
+elif total==5:
+    print(f"{die_1} & {die_2}: Little Phoebe!")
+elif total==4 and die_1==2:
+    print(f"{die_1} & {die_2}: Little Joe from Kokomo!")
+elif total==3:
+    print(f"{die_1} & {die_2}: Ace Caught a Deuce!")
+elif total==2:
+    print(f"{die_1} & {die_2}: Snake Eyes!")
+else:
+    print(f"{die_1} & {die_2}!")
+
