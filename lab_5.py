9b70d68bd7e9b803258605269315bd3dd6711e6f
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-13 19:37:36 -0500

    first commit

---
+++ lab_5.py
@@ -0,0 +1,13 @@
+"""
+Dice game
+Author: Dmitrii DOlgov
+Roll 2 dice, count the scores and print the funny names of combinations
+"""
+
+import random
+
+dice_1: int = random.randint(1,6)
+dice_2: int =random.randint(1,6)
+
+print(dice_1)
+print(dice_2)
