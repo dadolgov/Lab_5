8fd38c946e595d09ed8ac28a3af7b759e8f5cbe1
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-02-13 20:07:21 -0500

    added nickname table

--- lab_5.py
+++ lab_5.py
@@ -6,8 +6,32 @@ Roll 2 dice, count the scores and print the funny names of combinations
 
 import random
 
-dice_1: int = random.randint(1,6)
-dice_2: int =random.randint(1,6)
+die_1 : int = random.randint(1,6)
+die_2 : int = random.randint(1,6)
+total : int = die_1 + die_2
 
-print(dice_1)
-print(dice_2)
+print(die_1)
+print(die_2)
+
+if total==12: 
+    print("Boxcars")
+elif(total==11):
+    print("Six Five no Jive")
+elif total==10 and die_1==5:
+    print("Puppy Paws")
+elif total==9:
+    print("Nina from Pasadena")
+elif total==8 and die_1==4:
+    print("Eighter from Decatur")
+elif total==7 and (die_1==1 or die_2==1):
+    print("Six Ace")
+elif total==6 and die_1==3:
+    print("Jimmy Hicks from the Sticks")
+elif total==5:
+    print("Little Phoebe")
+elif total==4 and die_1==2:
+    print("Little Joe from Kokomo")
+elif total==3:
+    print("Ace Caught a Deuce")
+elif total==2:
+    print("Snake Eyes")
